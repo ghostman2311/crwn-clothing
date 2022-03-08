3f130cfebf7b7e5158c56f075ee20a6ad6229ab2
Nikhil <nkarnwal07@gmail.com>
2022-03-08 07:14:33 +0530

    updating the app with shop page ui, built the collection-item component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React from "react";
 import HomePage from "./pages/homepage/HomePage";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
-
-function HatsPage() {
-  return <div>Hats Page</div>;
-}
+import Shop from "./pages/shop/Shop";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/hats" component={HatsPage} />
+        <Route exact path="/shop" component={Shop} />
       </Switch>
     </div>
   );
---
+++ src/components/collection-preview/CollectionPreview.jsx
@@ -0,0 +1,20 @@
+import CollectionItem from "../collection-item/CollectionItem";
+import "./collectionpreview.scss";
+
+const CollectionPreview = ({ id, title, items }) => {
+  console.log(items);
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(({ id, ...otherProps }) => (
+            <CollectionItem key={id} {...otherProps} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default CollectionPreview;
