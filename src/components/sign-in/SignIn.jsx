ba6f8e4726669cc6ef89e0d1fb7fe7f20d897384
Nikhil <nkarnwal07@gmail.com>
2022-03-12 14:27:02 +0530

    Sign in component styles added

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import FormInput from "../form-input/FormInput";
+import "./sign-in.scss";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -31,22 +33,22 @@ class SignIn extends React.Component {
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.onSubmitHandler}>
-          <input
+          <FormInput
             name="email"
             type="email"
             value={this.state.email}
             onChange={this.onChangeHandler}
+            label="Email"
             required
           />
-          <label>Email</label>
-          <input
+          <FormInput
             name="password"
             type="password"
             onChange={this.onChangeHandler}
             value={this.state.password}
+            label="Password"
             required
           />
-          <label>Password</label>
           <input type="submit" value="Submit Form" />
         </form>
       </div>
